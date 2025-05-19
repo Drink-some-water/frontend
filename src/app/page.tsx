a975a35d0dd52b44e9a62d86659bbda863b34f77
Drink-some-water <seancollins362@gmail.com>
2025-05-19 13:28:59 -0700

    removing unnecessary styling || changed frontend title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Mp3UploadBox from './components/client/mp3-upload-box'
 import DragDropHandler from './components/client/drag-drop-handler'
+import "./globals.css";
 
 export default function Home() {
   return (
